42e8e2bd1827dae5b263d1a5864d5c523299f906
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-03-25 23:43:24 +0500

    updated

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,44 @@
-const Profile = () => {
+"use client"
+
+import { useState, useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import Profile from "@components/Profile"
+
+
+const MyProfile = () => {
+
+    const { data: session } = useSession();
+    const [posts, setPosts] = useState([])
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch(`/api/users/${session?.user.id}/posts`);
+            const data = await response.json();
+
+            setPosts(data);
+        }
+        if (session?.user.id) fetchPosts();
+    }, [])
+
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
+    }
+
+    const handleDelete = async (post) => {
+
+    }
+
     return (
-        <div>Profile</div>
+        <Profile
+            name="My"
+            desc="Welcome to Your Profile!"
+            data={posts}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+        />
     )
 }
 
-export default Profile
+export default MyProfile
